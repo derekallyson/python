9c2857f46c3121628a8f903c6f72868ff567ce39
Derek Allyson <derekallyson@gmail.com>
2024-06-04 15:05:32 +0000

    Using hello to connect to the db and do a global select

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print('hello, world!')
+
+import sqlite3 
+  
+# Connecting to sqlite 
+# connection object 
+connection_obj = sqlite3.connect('dummy.db') 
+  
+# cursor object 
+cursor_obj = connection_obj.cursor() 
+  
+# to select all column we will use 
+statement = '''SELECT * FROM dummy'''
+  
+cursor_obj.execute(statement) 
+  
+print("All the data") 
+output = cursor_obj.fetchall() 
+for row in output: 
+  print(row) 
+  
+connection_obj.commit() 
+  
+# Close the connection 
+connection_obj.close()
